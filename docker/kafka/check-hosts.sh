1f86fda36d09463f649f06c3b686bf4a7ba07164
Radosław Szmit <radoslawszmit@gmail.com>
2019-07-11 15:57:21 +0200

    Kafka Manager

--- docker/kafka/check-hosts.sh
+++ docker/kafka/check-hosts.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-docker exec -it cluster_zookeeper ip r | grep src
+docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' cluster_zookeeper
 
-docker exec -it cluster_kafka1 ip r | grep src
+docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' cluster_kafka1
 
-docker exec -it cluster_kafka2 ip r | grep src
+docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' cluster_kafka2
 
-docker exec -it cluster_kafka3 ip r | grep src
+docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' cluster_kafka3
+
+docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' kafka_manager
