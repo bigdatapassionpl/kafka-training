a6c40f7037b4b849e244c312fdf7a92a5f282ec1
Radosław Szmit <radoslawszmit@gmail.com>
2020-03-19 09:18:39 +0100

    Kafka Streams

---
+++ src/main/java/com/bigdatapassion/streams/WordCountWindowApplication.java
@@ -0,0 +1,54 @@
+package com.bigdatapassion.streams;
+
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.*;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.regex.Pattern;
+
+import static com.bigdatapassion.KafkaConfigurationFactory.TOPIC;
+import static com.bigdatapassion.KafkaConfigurationFactory.getStreamConfig;
+
+public class WordCountWindowApplication {
+
+    private static final Pattern PATTERN = Pattern.compile("\\W+");
+
+    public static void main(String[] args) {
+
+        Properties config = getStreamConfig();
+
+        StreamsBuilder builder = new StreamsBuilder();
+
+        KStream<String, String> textLines = builder.stream(TOPIC);
+
+        KTable<Windowed<String>, Long> wordCountTable = textLines
+                .flatMapValues(value -> Arrays.asList(PATTERN.split(value.toLowerCase())))
+                .filter((key, value) -> value.contains("psa"))
+                .groupBy((key, word) -> word)
+                .windowedBy(TimeWindows.of(10))
+                .count(Materialized.as("counts-store"));
+
+        KStream<Windowed<String>, Long> wordCountStream = wordCountTable.toStream();
+
+        wordCountStream.print(Printed.<Windowed<String>, Long>toSysOut().withLabel("Counted Parts"));
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), config);
+
+        // Nie używać na produkcji, czyści stan strumieni
+        // streams.cleanUp();
+
+        // start Kafka Streams
+        streams.start();
+
+        // jeśli chcemy zamknać aplikację po jakimś czasie to najprościej dajemy sleep i close
+        // Thread.sleep(5000L);
+        // streams.close();
+
+        // Eleganckie zamknięcie
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+    }
+
+}
