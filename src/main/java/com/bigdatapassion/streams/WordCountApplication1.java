73704ca6258a3b10e0ce2f1a52771ffb66c50572
Radosław Szmit <radoslawszmit@gmail.com>
2021-08-29 13:34:17 +0200

    Refactoring

--- src/main/java/com/bigdatapassion/streams/WordCountApplication1.java
+++ src/main/java/com/bigdatapassion/streams/WordCountApplication1.java
@@ -14,39 +14,37 @@ import static com.bigdatapassion.KafkaConfigurationFactory.*;
 
 public class WordCountApplication1 {
 
-    private static final String PATTERN = "\\W+";
+    private static final String WORD_SPLIT_PATTERN = "\\W+";
+    private static final String APPLICATION_NAME = "word-count";
+    private static final String COUNTS_STORE = APPLICATION_NAME + "-store";
 
     public static void main(final String[] args) {
 
         Properties config = getStreamConfig();
-        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount");
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, APPLICATION_NAME);
 
         StreamsBuilder builder = new StreamsBuilder();
 
         KStream<String, String> inputStreamWithMessages = builder.stream(TOPIC);
 
         KTable<String, Long> wordCountTable = inputStreamWithMessages
-                .mapValues((ValueMapper<String, String>) String::toLowerCase)
-                .flatMapValues(textLine -> Arrays.asList(textLine.split(PATTERN)))
-                .through(TOPIC_THROUGH)
-                //.map((key, value) -> new KeyValue<>(value, value))
-                .selectKey((key, value) -> value)
-//                .peek((key, value) -> System.out.println(String.format("(key:%s -> value:%s)", key, value)))
-                .groupByKey()
-                .count(Materialized.as("counts-store"));
+                .mapValues((ValueMapper<String, String>) String::toLowerCase) // --> to lower case
+                .flatMapValues(textLine -> Arrays.asList(textLine.split(WORD_SPLIT_PATTERN))) // -> to words very simple convert
+//                .map((key, value) -> new KeyValue<>(value, value)) // --> mapping value as key (like below)
+                .selectKey((key, value) -> value) // --> mapping value as key (like above)
+//                .through(TOPIC_THROUGH) // --> writing curent values to topic
+//                .peek((key, value) -> System.out.println(String.format("(key:%s -> value:%s)", key, value))) // --> printing current values, debug purpose
+                .groupByKey() // --> group stream by keys
+                .count(Materialized.as(COUNTS_STORE));
 
-        KStream<String, Long> wordCountStream = wordCountTable.toStream();
-
-        wordCountStream.to(TOPIC_OUT, Produced.with(Serdes.String(), Serdes.Long()));
+        wordCountTable.toStream().to(TOPIC_OUT, Produced.with(Serdes.String(), Serdes.Long())); // --> konwerting KTable to regular stream and save in output topic
 
         Topology topology = builder.build();
         KafkaStreams streams = new KafkaStreams(topology, config);
 
-        // Nie używać na produkcji, czyści stan strumieni
-        // streams.cleanUp();
+//         streams.cleanUp(); // cleaning current store state !!!
 
-        // start Kafka Streams
-        streams.start();
+        streams.start(); // start Kafka Streams App
 
         // Print topology
         System.out.println("TOPOLOGY:");
