d8c8a8375b9b81ee95202b18453efe5e32c95db3
Radosław Szmit <radoslawszmit@gmail.com>
2021-09-24 19:39:40 +0200

    Refactoring

--- src/main/java/com/bigdatapassion/streams/WordCountApplication1.java
+++ src/main/java/com/bigdatapassion/streams/WordCountApplication1.java
@@ -15,8 +15,8 @@ import static com.bigdatapassion.KafkaConfigurationFactory.*;
 public class WordCountApplication1 {
 
     private static final String WORD_SPLIT_PATTERN = "\\W+";
-    private static final String APPLICATION_NAME = "word-count";
-    private static final String COUNTS_STORE = APPLICATION_NAME + "-store";
+    private static final String APPLICATION_NAME = "wordcount-application";
+    private static final String COUNTS_STORE = "wordcount-store";
 
     public static void main(final String[] args) {
 
@@ -35,20 +35,27 @@ public class WordCountApplication1 {
 //                .through(TOPIC_THROUGH) // --> writing curent values to topic
 //                .peek((key, value) -> System.out.println(String.format("(key:%s -> value:%s)", key, value))) // --> printing current values, debug purpose
                 .groupByKey() // --> group stream by keys
-                .count(Materialized.as(COUNTS_STORE));
+                .count(Materialized.as(COUNTS_STORE)); // --> saving state to topic: ${applicationId}-${internalStoreName}-changelog
 
         wordCountTable.toStream().to(TOPIC_OUT, Produced.with(Serdes.String(), Serdes.Long())); // --> konwerting KTable to regular stream and save in output topic
 
         Topology topology = builder.build();
         KafkaStreams streams = new KafkaStreams(topology, config);
 
-//         streams.cleanUp(); // cleaning current store state !!!
+        // Delete the application's local state.
+        // Note: In real application you'd call `cleanUp()` only under certain conditions.
+        // See Confluent Docs for more details:
+        // https://docs.confluent.io/current/streams/developer-guide/app-reset-tool.html#step-2-reset-the-local-environments-of-your-application-instances
+//        streams.cleanUp();
 
         streams.start(); // start Kafka Streams App
 
         // Print topology
         System.out.println("TOPOLOGY:");
         System.out.println(topology.describe());
+
+        System.out.println("TOPOLOGY:");
+        System.out.println(streams.toString());
 
         System.out.println("TOPOLOGY:");
         streams.localThreadsMetadata().forEach(System.out::println);
