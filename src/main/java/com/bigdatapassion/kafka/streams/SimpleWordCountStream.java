91f96b1181df98a6e2d734688fddaaeeb6c71be7
Radosław Szmit <radoslawszmit@gmail.com>
2021-11-24 21:30:05 +0100

    Kafka Streams

--- src/main/java/com/bigdatapassion/kafka/streams/SimpleWordCountStream.java
+++ src/main/java/com/bigdatapassion/kafka/streams/SimpleWordCountStream.java
@@ -2,13 +2,12 @@ package com.bigdatapassion.kafka.streams;
 
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.*;
 
 import java.util.Arrays;
-import java.util.Properties;
 
-import static com.bigdatapassion.kafka.conf.KafkaConfigurationFactory.*;
+import static com.bigdatapassion.kafka.conf.KafkaConfigurationFactory.TOPIC_SIMPLE;
+import static com.bigdatapassion.kafka.conf.KafkaConfigurationFactory.TOPIC_SIMPLE_WORDCOUNT;
 
 public class SimpleWordCountStream extends KafkaStreamsApp {
 
@@ -34,7 +33,9 @@ public class SimpleWordCountStream extends KafkaStreamsApp {
                 .groupByKey() // --> group stream by keys
                 .count(Materialized.as(COUNT_STORE)); // --> saving state to topic: ${applicationId}-${internalStoreName}-changelog
 
-        wordCountTable.toStream().to(TOPIC_SIMPLE_WORDCOUNT, Produced.with(Serdes.String(), Serdes.Long())); // --> konwerting KTable to regular stream and save in output topic
+        KStream<String, Long> wordCountStream = wordCountTable.toStream();
+
+        wordCountStream.to(TOPIC_SIMPLE_WORDCOUNT, Produced.with(Serdes.String(), Serdes.Long())); // --> konwerting KTable to regular stream and save in output topic
     }
 
 }
--- src/main/java/com/bigdatapassion/kafka/streams/SimpleWordCountStream2.java
+++ src/main/java/com/bigdatapassion/kafka/streams/SimpleWordCountStream2.java
@@ -1,7 +1,6 @@
 package com.bigdatapassion.kafka.streams;
 
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
@@ -9,46 +8,33 @@ import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.Produced;
 
 import java.util.Arrays;
-import java.util.Properties;
 import java.util.regex.Pattern;
 
-import static com.bigdatapassion.kafka.conf.KafkaConfigurationFactory.*;
+import static com.bigdatapassion.kafka.conf.KafkaConfigurationFactory.TOPIC_SIMPLE;
+import static com.bigdatapassion.kafka.conf.KafkaConfigurationFactory.TOPIC_SIMPLE_WORDCOUNT;
 
-public class WordCountApplication2 {
+public class SimpleWordCountStream2 extends KafkaStreamsApp {
+
+    private static final String COUNT_STORE = "count-store";
 
     private static final Pattern PATTERN = Pattern.compile("\\W+");
 
     public static void main(String[] args) {
+        new SimpleWordCountStream2().run();
+    }
 
-        Properties config = getStreamConfig();
-
-        StreamsBuilder builder = new StreamsBuilder();
-
+    @Override
+    protected void createTopology(StreamsBuilder builder) {
         KStream<String, String> textLines = builder.stream(TOPIC_SIMPLE);
 
         KTable<String, Long> wordCountTable = textLines
                 .flatMapValues(value -> Arrays.asList(PATTERN.split(value.toLowerCase())))
                 .groupBy((key, word) -> word)
-                .count(Materialized.as("counts-store"));
+                .count(Materialized.as(COUNT_STORE));
 
         KStream<String, Long> wordCountStream = wordCountTable.toStream();
 
         wordCountStream.to(TOPIC_SIMPLE_WORDCOUNT, Produced.with(Serdes.String(), Serdes.Long()));
-
-        KafkaStreams streams = new KafkaStreams(builder.build(), config);
-
-        // Nie używać na produkcji, czyści stan strumieni
-        // streams.cleanUp();
-
-        // start Kafka Streams
-        streams.start();
-
-        // jeśli chcemy zamknać aplikację po jakimś czasie to najprościej dajemy sleep i close
-        // Thread.sleep(5000L);
-        // streams.close();
-
-        // Eleganckie zamknięcie
-        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
     }
 
 }
