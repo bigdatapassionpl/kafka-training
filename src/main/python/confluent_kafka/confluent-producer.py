9c64050a6799a9b0471b4100ce2a09558c5e8424
Radosław Szmit <radoslawszmit@gmail.com>
2025-06-10 14:03:59 +0200

    Kafka Python

--- src/main/python/confluent_kafka/confluent-producer.py
+++ src/main/python/confluent_kafka/confluent-producer.py
@@ -1,20 +1,30 @@
 import configparser
 import json
+import sys
 import time
 
 from confluent_kafka import Producer
 
+print("All arguments:", sys.argv)
+if len(sys.argv) > 2:
+    configPath = sys.argv[1]
+    configName = sys.argv[2]
+    print(f"configPath: {configPath}, configName: {configName}")
+else:
+    print("Wrong number of arguments!")
+    sys.exit(1)
+
 kafka_topic = 'confluent-kafka-python-example-topic'
 
 config = configparser.ConfigParser()
-config.read('/Users/radek/programs/kafka/config.properties')
+config.read(configPath)
 
 conf = {
-    'bootstrap.servers': 'bootstrap.mytestkafkacluster.europe-west3.managedkafka.bigdataworkshops.cloud.goog:9092',
-    'security.protocol': config['default']['security.protocol'],
-    'sasl.mechanism': config['default']['sasl.mechanism'],
-    'sasl.username': config['default']['username'],
-    'sasl.password': config['default']['password']
+    'bootstrap.servers': config[configName]['bootstrap.servers'],
+    'security.protocol': config[configName]['security.protocol'],
+    'sasl.mechanism': config[configName]['sasl.mechanism'],
+    'sasl.username': config[configName]['username'],
+    'sasl.password': config[configName]['password']
 }
 
 producer = Producer(conf)
