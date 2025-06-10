56ac17c111928d46fbf9c1619695a360fcf83acc
Radosław Szmit <radoslawszmit@gmail.com>
2025-06-10 14:50:01 +0200

    Kafka Python

--- src/main/python/config-test.py
+++ src/main/python/config-test.py
@@ -1,7 +1,16 @@
 import configparser
+import sys
+
+print("All arguments:", sys.argv)
+if len(sys.argv) > 1:
+    configPath = sys.argv[1]
+    print(f"configPath: {configPath}")
+else:
+    print("Wrong number of arguments!")
+    sys.exit(1)
 
 config = configparser.ConfigParser()
-config.read('/Users/radek/programs/kafka/config.properties')
+config.read(configPath)
 
 # Print all sections and their contents
 for section_name in config.sections():
