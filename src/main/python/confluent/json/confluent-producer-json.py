22effcd507f8e12041c442dc89b69aa62f0538b9
Radosław Szmit <radoslawszmit@gmail.com>
2026-01-28 18:06:08 +0100

    Kafka Python

--- src/main/python/confluent/json/confluent-producer-json.py
+++ src/main/python/confluent/json/confluent-producer-json.py
@@ -1,4 +1,5 @@
 import configparser
+from datetime import datetime
 import json
 import sys
 import time
@@ -29,10 +30,18 @@ for key, value in config[configName].items():
 producer = Producer(conf)
 
 for x in range(600000):
-    message = {'key': f'value{x}'}
+    message = {
+        'key': f'key_{x}',
+        'message_time': datetime.now().isoformat(),
+        'data': {
+            'id': x,
+            'name': f'name_{x}'
+        }
+    }
     print(f"Sending message: {message}")
     message_str = json.dumps(message).encode('utf-8')
     producer.produce(kafka_topic, value=message_str)
     time.sleep(1)
+    # time.sleep(0.01)
 
 producer.flush()
